ad491112faed0240ade3defa3dba8e0e21c9b7ba
Christoffer Lucas <christoffer.luccas@gmail.com>
2019-08-30 23:04:44 -0300

    adicionando projeto para estudo do componente navigation

---
+++ study-navigation-component/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+Y
+)app/src/main/res/layout/activity_main.xml,7\6\76b07e40f24cce093536731477540f12e7da3cb6
+h
+8app/src/main/java/br/xplorer/studynavcom/MainActivity.kt,6\b\6b379eea775a79d50e625a4f1758c444405a39d5
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
---
+++ study-navigation-component/app/src/main/java/br/xplorer/studynavcom/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.xplorer.studynavcom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Esse projeto tem como base o tutorial do link abaixo
+ * https://codelabs.developers.google.com/codelabs/android-navigation/#0
+ *
+ *
+ * */
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
